f0ffa24b76375893b9b6561a3ded6d40be39f835
larsonscott89 <scottlarson0305@gmail.com>
2024-02-15 14:00:56 -0500

    we have a dot

--- client/script.js
+++ client/script.js
@@ -1,77 +1,77 @@
 
-// const ruffoniButton = document.getElementById('ruffoniButton')
-// const mauvielButton = document.getElementById('mauvielButton')
-// const greenpanButton = document.getElementById('greenpanButton')
-// const allCladButton = document.getElementById('allcladButton')
+const ruffoniButton = document.getElementById('ruffoniButton')
+const mauvielButton = document.getElementById('mauvielButton')
+const greenpanButton = document.getElementById('greenpanButton')
+const allCladButton = document.getElementById('allcladButton')
 const zwillingButton = document.getElementById('zwillingButton')
-// const searchInput = document.getElementById('inputBar')
-// //const searchResults = document.getElementById('searchResults')
-// const searchButton = document.getElementById('searchButton')
+const searchInput = document.getElementById('inputBar')
+//const searchResults = document.getElementById('searchResults')
+const searchButton = document.getElementById('searchButton')
 const setList = document.querySelector('#setsContainer')
 
-// const BASE_URL = 'http://localhost:3001/'
-// let sets
+const BASE_URL = 'http://localhost:3001/'
+let sets
 
-// const getSet = async (input) => {
-//     sets = await axios.get(`http://localhost:3001/sets/${input}`)
-//     console.log(sets.data.results)
-// }
+const getSet = async (input) => {
+    sets = await axios.get(`http://localhost:3001/sets/${input}`)
+    console.log(sets.data.results)
+}
 
-// // const set= document.querySelectorAll(`.sets`)
-// // const itemName =document.querySelectorAll(".setName")
-// // const itemQuantity= document.querySelectorAll(".itemQuantity")
-// // const itemSummary = document.querySelectorAll(".itemSummary")
-// // const itemPicture = document.querySelectorAll(`.image`) 
-// // const itemType = document.querySelectorAll(".itemType")
-// // const itemColours =document.querySelectorAll(".itemColours")
-// // const itemPrice = document.querySelectorAll(".itemPrice")
-// // const itemUse = document.querySelectorAll(".itemUse")
-// // const itemCare = document.querySelectorAll(".itemCare")
-// // const itemBrand = document.querySelectorAll(".itemBrand")
+// const set= document.querySelectorAll(`.sets`)
+// const itemName =document.querySelectorAll(".setName")
+// const itemQuantity= document.querySelectorAll(".itemQuantity")
+// const itemSummary = document.querySelectorAll(".itemSummary")
+// const itemPicture = document.querySelectorAll(`.image`) 
+// const itemType = document.querySelectorAll(".itemType")
+// const itemColours =document.querySelectorAll(".itemColours")
+// const itemPrice = document.querySelectorAll(".itemPrice")
+// const itemUse = document.querySelectorAll(".itemUse")
+// const itemCare = document.querySelectorAll(".itemCare")
+// const itemBrand = document.querySelectorAll(".itemBrand")
 
 
-// const appendSets = () => {
-//     console.log(setsList)
-//     const listItem = document.createElement("li")
-//     const itemName = document.createElement("p")
-//     const itemPicture = document.createElement("img")
-//     const itemQuantity= document.createElement("p")
-//     const itemSummary = document.createElement("p") 
-//     const itemType = document.createElement("p")
-//     const itemColours =ddocument.createElement("p")
-//     const itemPrice = ddocument.createElement("p")
-//     const itemUse = document.createElement("p")
-//     const itemCare = document.createElement("p")
-//     const itemBrand = document.createElement("p")
+const appendSets = () => {
   
-// // itemPicture.className ="itemPicture"
-// // itemName.className ="itemName"
-// // itemQuantity.className ="itemQUantity"
-// // itemSummary.className ="itemSummary"
-// // itemType.className ="itemType"
-// // itemColours.className ="itemColours"
-// // itemPrice.className ="itemPrice"
-// // itemUse.className ="itemUse"
-// // itemCare.className ="itemCare"
-// // itemBrand.className ="itemBrand"
+    const listItem = document.createElement("li")
+    const itemName = document.createElement("p")
+    const itemPicture = document.createElement("img")
+    const itemQuantity= document.createElement("p")
+    const itemSummary = document.createElement("p") 
+    const itemType = document.createElement("p")
+    const itemColours =document.createElement("p")
+    const itemPrice = document.createElement("p")
+    const itemUse = document.createElement("p")
+    const itemCare = document.createElement("p")
+    const itemBrand = document.createElement("p")
+  
+itemPicture.className ="itemPicture"
+itemName.className ="itemName"
+itemQuantity.className ="itemQUantity"
+itemSummary.className ="itemSummary"
+itemType.className ="itemType"
+itemColours.className ="itemColours"
+itemPrice.className ="itemPrice"
+itemUse.className ="itemUse"
+itemCare.className ="itemCare"
+itemBrand.className ="itemBrand"
 
 
        
-// listItem.appendChild(itemPicture)
-// listItem.appendChild(itemName)
-// listItem.appendChild(itemQuantity)
-// listItem.appendChild(itemSummary)
-// listItem.appendChild(itemType)
-// listItem.appendChild(itemColours)
-// listItem.appendChild(itemPrice)
-// listItem.appendChild(itemUse)
-// listItem.appendChild(itemCare)
-// listItem.appendChild(itemBrand)
+listItem.appendChild(itemPicture)
+listItem.appendChild(itemName)
+listItem.appendChild(itemQuantity)
+listItem.appendChild(itemSummary)
+listItem.appendChild(itemType)
+listItem.appendChild(itemColours)
+listItem.appendChild(itemPrice)
+listItem.appendChild(itemUse)
+listItem.appendChild(itemCare)
+listItem.appendChild(itemBrand)
 
-// setList.appendChild(listItem)
-// console.log(setList)
+setList.appendChild(listItem)
+console.log(setList)
 
-// }
+}
        
 
 
@@ -164,46 +164,62 @@ const setList = document.querySelector('#setsContainer')
 // // })
 // //Grab Sets
 
-zwillingButton.addEventListener('click', async() => {
-    const response = await axios.get(`http://localhost:3001/sets/Zwilling`)
-    console.log(response)
+// zwillingButton.addEventListener('click', async() => {
+//     const response = await axios.get(`http://localhost:3001/sets/Zwilling`)
+//     console.log(response.data)
     // const data = response.data
     // console.log(data)
     // for (let i=0; i<response.data.length; i++) {
     //     console.log(response.data.set[i])
     // }
 
-})
+// })
 
-// zwillingButton.addEventListener('click', async () => {
-//         // sets.preventDefault();
-//         let input = zwillingButton.innerHTML
-//         const response = await axios.get(`${BASE_URL}/sets/Zwilling`);
-//         // const setDataList = response.data.sets || [];
-//         // setList.innerHTML = ""
-//         console.log(response)
+zwillingButton.addEventListener('click', async () => {
+        // sets.preventDefault();
+        let input = zwillingButton.innerHTML
+        const response = await axios.get(`${BASE_URL}sets/Zwilling`);
+        const setDataList = response.data.sets || [];
+        setList.innerHTML = ""
+       
+        console.log(response.data)
         
-//     for (let i = 0; i < response.data.length ; i++) {
-//             appendSets()
+    // for (let i = 0; i < response.data.length ; i++) {
+    //   const varName = setlist.appendChild(document.createElement('p'))
+    //   varName.textContent = response.data[i].set
+    const listItem = document.createElement("li")
+    const itemName = document.createElement("p")
+    const itemPicture = document.createElement("img")
+    const itemQuantity= document.createElement("p")
+    const itemSummary = document.createElement("p") 
+    const itemType = document.createElement("p")
+    const itemColours =document.createElement("p")
+    const itemPrice = document.createElement("p")
+    const itemUse = document.createElement("p")
+    const itemCare = document.createElement("p")
+    const itemBrand = document.createElement("p")
+
+      
+            appendSets()
            
-//             itemPicture.src = response.data.picture
-//             itemName.textContent = response.data.set
-//             itemQuantity.textContent = response.data.quantity
-//             itemSummary.textContent = response.data.summary
-//             itemType.textContent = response.data.type
-//             // itemBrand.textContent = sets.data.brand
-//             // itemPrice.textContent =sets.data.price
-//             // for (let b = 0; b < sets.data.length ; b++) {
-//             // itemUse.innerHTML += `${sets.data.use[b]}`}
-//             // for (let b = 0;  b< sets.data.length ; b++) {
-//             // itemCare.innerHTML +=`${sets.data.care[b]}`}
-//             // for (let b = 0;  b< sets.data.length ; b++) {
-//             // itemColours.innerHTML += `${sets.data.colours[b]}`
+            // itemPicture.src = response.data.picture
+            itemName.textContent = response.data.set
+            itemQuantity.textContent = response.data.quantity
+            itemSummary.textContent = response.data.summary
+            itemType.textContent = response.data.type
+            // itemBrand.textContent = sets.data.brand
+            // itemPrice.textContent =sets.data.price
+            // for (let b = 0; b < sets.data.length ; b++) {
+            // itemUse.innerHTML += `${sets.data.use[b]}`}
+            // for (let b = 0;  b< sets.data.length ; b++) {
+            // itemCare.innerHTML +=`${sets.data.care[b]}`}
+            // for (let b = 0;  b< sets.data.length ; b++) {
+            // itemColours.innerHTML += `${sets.data.colours[b]}`
             
-//         // }
-//     }
-// }
-// )
+        // }
+    }
+// } 
+)
 // //    setDataList.forEach(sets => {
 // //         appendSets(sets)
         
